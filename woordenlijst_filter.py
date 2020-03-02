e9ba2b62bd467472a1d51bd08485b0fbbe827033
hacxpiont <huubexel@hotmail.com>
2020-03-02 16:22:40 +0100

    Afmaken, dit ding filtert de lijst met woorden die goed zijn.

---
+++ woordenlijst_filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" dit programma filtert alle woorden op woorden die voldoen aan de specificaties en haalt woorden die niet voldoen ertussenuit"""
+
+def main():
+    with open('woorden.txt', 'r') as w:
+        data = w.readlines()
+
+
+if __name__ == "__main__"
+    main()
