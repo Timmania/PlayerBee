0262b6f3fd90727634de45a4cfc2bfde95aa7b69
Timmania <timmania@solcon.nl>
2020-03-05 12:29:06 +0100

    modify Gamestate.py
    make set_letters random
    fix scoring system

--- Project/interface/Gamestate.py
+++ Project/interface/Gamestate.py
@@ -1,23 +1,34 @@
 # from woordenlijst import word_set
+import random
 
 
 class GameState:
     def __init__(self):
-        self.letters = ["b", "p", "l", "a", "y", "e", "r"]
+        self.letters = []
         self.score = 0
-        self.word_set = {}
+        self.word_set = set()  # TODO link to wordlist to get set of words with self.letters
+        self.pangram_set = set()
+
+        self.set_letters()
 
     def get_letters(self):
         return self.letters
 
-    def set_letters(self, letters):
-        self.letters = letters
+    def set_letters(self):
+        new = random.choice(self.pangram_set)
+        self.pangram_set.remove(new)
+        self.letters = new
 
     def increase_score(self, word):
         if len(word) == 4:
             self.score += 1
-        elif len(word):
-            pass
+            return "+1"
+        elif sorted(list(word)) == sorted(self.letters):
+            self.score += 14
+            return "PANGRAM FOUND! +14"
+        else:
+            self.score += len(word)
+            return "+" + str(len(word))
 
     def is_correct(self, guess):
         if not all(x in self.letters for x in list(guess)):
@@ -26,4 +37,4 @@ class GameState:
             return "Middle letter not in guess"
         if guess not in self.word_set:
             return "Word not in word set"
-        return True
+        return self.increase_score(guess)
