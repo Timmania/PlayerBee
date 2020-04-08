a5f6e0eba64123d764b86cbf66bf7a964dc53231
hacxpiont <huubexel@hotmail.com>
2020-04-08 13:21:28 +0200

    Dit ding gaat net zo lang door totdat hij een woord vind

--- Project/interface/puzzle_solver.py
+++ Project/interface/puzzle_solver.py
@@ -4,27 +4,27 @@ Wat dit programma doet is het pakt de 7 letters die in de hive staan en geeft ee
 """
 
 from random import *
+from Project.interface.Gamestate import GameState
 
 
-def find_match(letters_in_hive):
+def find_match(letters):
     """
     Deze functie gaat net zo lang door totdat de guess een daadwerkelijk bestaand woord is.
 
     """
     guess = []
     while guess not in possible_words:              # possible_words zijn hier de woorden die je mogelijk kunt maken met de 7 letters
-        guess = give_guess(letters_in_hive)
+        guess = give_guess(letters)
     return guess
 
 
-def give_guess(letters_in_hive):
+def give_guess(letters):
     """
     Deze functie neemt de letters in de hive en geeft met deze letters een guess in deze voor de letters in deze hive
 
     """
-    # letters_in_hive is een list, set, tuple of dictionary
     if random() < 0.8:
         amount_of_letters = randint(4, 11)
     else:
         amount_of_letters = randint(10, 22)
-    return [choice(letters_in_hive) for i in range(amount_of_letters)]
+    return [choice(letters) for i in range(amount_of_letters)]
