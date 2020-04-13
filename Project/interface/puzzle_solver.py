fe8f74ddec21bf19da2086d126785ba350c14f17
hacxpiont <huubexel@hotmail.com>
2020-04-13 20:55:54 +0200

    Heb alles pep 8 gemaakt

--- Project/interface/puzzle_solver.py
+++ Project/interface/puzzle_solver.py
@@ -15,7 +15,8 @@ def find_match(letters):
     :param: The 7 letters that are on screen when playing the game
     :return: A guess that is actually in the list, so a right guessed guess
     """
-    # possible_words are the words that you can possibly make with the 7 letters on screen when playing
+    # possible_words are the words that you can possibly make
+    # with the 7 letters on screen when playing
     guess = []
     while guess not in possible_words(letters):
         guess = give_guess(letters)
@@ -24,19 +25,24 @@ def find_match(letters):
 
 def give_guess(letters):
     """
-    This function returns a guess that could possibly be a word in the list of possible_words.
+    This function returns a guess that could possibly be a word
+    in the list of possible_words.
     :param: The 7 letters that are on screen when playing the game
     :return: returns a guess
     """
-    if random() < 0.8:                          # the change that a word has 10 or more letters is very small so,
-        amount_of_letters = randint(4, 11)      # to make the system faster i gave it 20% change to be a large word.
+    if random() < 0.8:
+        # the change that a word has or more letters is very small so,
+        amount_of_letters = randint(4, 11)
+        # to make the system faster i gave it 20% change to be a large word.
     else:
         amount_of_letters = randint(10, 22)
     guess = [choice(letters) for i in range(amount_of_letters)]
-    if letters[0] not in guess:                 # Middle letter of hive has to be in guess (in this case letters[0])
+    if letters[0] not in guess:
+        # Middle letter of hive has to be in guess (in this case letters[0])
         guess.extend(letters[0])
         shuffle(guess)
-    return "".join(guess)                       # returns guess as string
+    return "".join(guess)
+    # returns guess as string
 
 
 def main():
